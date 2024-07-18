c908c9977e49ed7db203fb240f938367a0cfa7bc
Luca-Mariselli <lucamariselli@gmail.com>
2024-07-18 11:41:27 +0200

    add api project controller

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    public function index(){
+        return  response()->json([
+            'success' => true,
+            'projects' => Project::orderBy('id')->paginate()
+        ]);
+    }
+}
